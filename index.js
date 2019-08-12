3299cc4cd8189c4125ab4bb96f551e2c966b8eae
Corentin H <1763364+viki53@users.noreply.github.com>
2019-08-12 12:00:03 +0200

    Convert to ES6

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-'use strict';
-
-function fuzzysearch (needle, haystack) {
-  var hlen = haystack.length;
-  var nlen = needle.length;
-  if (nlen > hlen) {
-    return false;
-  }
-  if (nlen === hlen) {
-    return needle === haystack;
-  }
-  outer: for (var i = 0, j = 0; i < nlen; i++) {
-    var nch = needle.charCodeAt(i);
-    while (j < hlen) {
-      if (haystack.charCodeAt(j++) === nch) {
-        continue outer;
-      }
-    }
-    return false;
-  }
-  return true;
+export default function fuzzysearch (needle, haystack) {
+	const hlen = haystack.length;
+	const nlen = needle.length;
+	if (nlen > hlen) {
+		return false;
+	}
+	if (nlen === hlen) {
+		return needle === haystack;
+	}
+	outer: for (var i = 0, j = 0; i < nlen; i++) {
+		var nch = needle.charCodeAt(i);
+		while (j < hlen) {
+			if (haystack.charCodeAt(j++) === nch) {
+				continue outer;
+			}
+		}
+		return false;
+	}
+	return true;
 }
-
-module.exports = fuzzysearch;
